2060d10e51da4c43d1484423f63764f9cbe56c4c
Manuel Alvarez <manu_07_alvarez@hotmail.com>
2023-03-11 04:17:20 -0300

    Login view setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'cv', pathMatch: 'full' },
+  { path: 'cv', loadChildren: () => import('./cv/cv.module').then(m => m.CvModule) },
+  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-   declarations: [
-  ]
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,21 @@
-
+import { CvModule } from './cv/cv.module';
+import { LoginModule } from './login/login.module';
 import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { ButtonfacebookComponent } from './components/buttons/buttonfacebook/buttonfacebook.component';
-import { ButtontwitterComponent } from './components/buttons/buttontwitter/buttontwitter.component';
-import { ButtoninstagramComponent } from './components/buttons/buttoninstagram/buttoninstagram.component';
-import { ButtonlinkedinComponent } from './components/buttons/buttonlinkedin/buttonlinkedin.component';
-import { ButtongithubComponent } from './components/buttons/buttongithub/buttongithub.component';
-import { ButtonloginComponent } from './components/buttons/buttonlogin/buttonlogin.component';
-import { AboutComponent } from './components/about/about.component';
-import { ExperienceComponent } from './components/experience/experience.component';
-import { EducationComponent } from './components/education/education.component';
-import { SkillsComponent } from './components/skills/skills.component';
-import { ProjectsComponent } from './components/projects/projects.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-      HeaderComponent,
-      ButtonfacebookComponent,
-      ButtontwitterComponent,
-      ButtoninstagramComponent,
-      ButtonlinkedinComponent,
-      ButtongithubComponent,
-      ButtonloginComponent,
-      AboutComponent,
-      ExperienceComponent,
-      EducationComponent,
-      SkillsComponent,
-      ProjectsComponent,
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    LoginModule,
+    CvModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
